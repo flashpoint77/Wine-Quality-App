04f8dfb9af5c063722ad92ba50e8f08c97eb3d93
flashpoint77 <51173362+flashpoint77@users.noreply.github.com>
2023-02-06 12:21:24 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,14 +4,14 @@ from markupsafe import escape
 import pickle
 
 # Create flask app
-flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route("/predict", methods = ["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
@@ -23,4 +23,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
